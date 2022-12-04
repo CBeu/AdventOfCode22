4bb5dce23c94b3d40e8ee034c51e390dd5d74772
Craig Beuerlein <cdbuilds@gmail.com>
2022-12-04 00:29:51 -0500

    Day 3 solved

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,23 +1,14 @@
 import os
 
+
 def loadData(path):
     with open(path,'r') as f:
         f_contents = f.read().splitlines()
         return f_contents
 
-class Group:
-    def __init__(self, r1, r2, r3):
-        self.r1 = r1
-        self.r2 = r2
-        self.r3 = r3
-        self.shared = self.shared()
-    
-    def shared(self):
-        r12 = ''.join(set(self.r1.shared).intersection(self.r2.shared))
-        r123 = ''.join(set(r12).intersection(self.r3.shared))
-
 class Rucksack:
     def __init__(self, items):
+        self.items = items
         self.firstHalf = items[:len(items)//2]
         self.secondHalf = items[len(items)//2:]
         self.firstComp = self.fillComp(self.firstHalf)
@@ -39,37 +30,51 @@ class Rucksack:
         return dic
     
     def __str__(self):
-        return f"shared:{self.sharedComp}"
+        return f"self:{self.firstHalf}{self.secondHalf} shared:{self.sharedComp}\n"
+
+class Group:
+    def __init__(self, g):
+        self.r1 = Rucksack(g[0])
+        self.r2 = Rucksack(g[1])
+        self.r3 = Rucksack(g[2])
+        self.shared = self.shared()
+        self.sharedSum = self.getCharPriority(self.shared)
+
+    def getCharPriority(self, items):
+        sums = 0
+        for i in items:
+            if i.isupper():
+                sums = sums + ((ord(i)-38))
+            else:
+                sums = sums +((ord(i)-96))
+        return sums
+    
+    def shared(self):
+        r123 = ''.join(set(set(self.r1.items).intersection(self.r2.items)).intersection(self.r3.items))
+        return r123
+    
+    def __str__(self):
+        return f"shared:{self.r1} {self.r2} {self.r3}"
 
 # Part 1
 def rucksackReorg(path):
     f = loadData(path)
-    rucksacks = []
+    sums = 0
     for line in f:
         r = Rucksack(line)
-        rucksacks.append(r)
-    
-    sums = 0
-    for r in rucksacks:
         sums = sums + r.sharedSum
     return sums
 
 # Part 2
 def findBadge(path):
     f = loadData(path)
-    groups = []
-    group = []
-    i = 0
-    for line in f:
-        if i <=2:
-            i = i + 1
-            group.append(Rucksack(line))
-        else:
-            groups.append(group)
-            group = []
-            i = 0
-    return groups
+    split = [f[i * 3:(i + 1) * 3] for i in range((len(f) + 3 - 1) // 3 )]
+    sums = 0
+    for i in split:
+        g = Group(i)
+        sums = sums + g.sharedSum
+    return sums
 
 
 print("Part1:\t",rucksackReorg('Day3/input.txt'))
-print("Part1:\t",findBadge('Day3/test.txt')[0])
+print("Part2:\t",(findBadge('Day3/input.txt')))
